098c3a40d982c99b14fda683f640c3dc5edfa325
Blake <russeth.blake@gmail.com>
2024-04-08 00:39:38 -0500

    chore: get rid of default component

--- frontend/src/app/weather-lookup/weather-lookup.component.ts
+++ frontend/src/app/weather-lookup/weather-lookup.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-weather-lookup',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './weather-lookup.component.html',
   styleUrl: './weather-lookup.component.css',
+  selector: 'weather-lookup',
 })
-export class WeatherLookupComponent {}
+export class WeatherLookupComponent {
+}
