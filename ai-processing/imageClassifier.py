cd173a7e95d3b40646b71697038a59b6ab5aafd0
Blake <brusseth@asu.edu>
2024-04-21 14:53:29 -0500

    feat(processing): add combos suggestion text generation, use meta's llama

--- ai-processing/imageClassifier.py
+++ ai-processing/imageClassifier.py
@@ -1,50 +1,39 @@
-from distutils.filelist import FileList
 from typing import List
-from aiohttp import FormData
 from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModelForCausalLM
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration, BlipTextModel
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/2")
+@app.route("/suggestCombos")
 def suggestCombos(clothingData: List[str] = [], weatherRating = ''):
 
+    import transformers
     import torch
 
-    suggestClassifier = pipeline('text-generation')
+    model_id = "meta-llama/Meta-Llama-3-8B"
 
-    prompt = '''you are a fashion designer who is skilled in creating an outfit. An outfit is defined as atleast one top piece like a shirt and one bottom like pants,
-    'but you can also add more than one or accessories like hats if they are a part of the items I provide you. 
-    Your skills include choosing color schemes for an outfit, considering the style of the outfit (like bojo or emo), and 
-     very importantly you need to consider the weather the outfit will be used in. I will also give you the weather of the user in 5 categories: very cold, cold, medium, hot, very hot. 
-     Here is the current weather rating I want you to consider 
-     {weatherRating} 
-      I will give you a list of items where each item is an article of clothing that was fed into another ml model to produce a 15 word descriptor of the clothing item. 
-      I want you to provide outfit combination recommendations based on the input of clothing items and weather. 
-      Return the outfits in a bulleted list and provide your justificaiton in 2 sentences under each. Provide the results in a format that can be easily parsed so I can extract the outfits to 
-      the user, 
-      include this as well as the bulleted list/description I described earlier. I will provide you the clothing items descriptions line by line here '''
-    
-    for item in clothingData:
-        prompt += '/n{item}'
+    pipeline = transformers.pipeline(
+        "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+    )
 
-    res = suggestClassifier(task='text2text-generation',inputs=prompt)
+
+
+    prompt = '''you are a fashion designer. you have a client who is looking for a new outfit. they are looking for something that is perfect for a {weatherRating} day. they have given you the following items of clothing to work with:
+    ''' + clothingData + ''' With this clothing data, I want you to supply me suggested outfit combinations based on the weather rating you are given. The weather rating you are given will be in a 5 scale, very cold/cold/medium/hot/very hot. Suggest as many combos as you feel appropriately fits the criteria.'''
+
+    res = pipeline(prompt)
+
 
     return res
 
 
 
-from flask import request
-from PIL import Image
-from transformers import set_seed
 @app.route("/", methods=['POST'])
 def classifyImages():
-    import requests
-    from PIL import Image
-    from transformers import BlipProcessor, BlipForConditionalGeneration
 
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
