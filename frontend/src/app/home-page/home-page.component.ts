ecf19c4e143910c1d3a54949098f3d94e855891d
Blake <brusseth@asu.edu>
2024-04-27 13:19:44 -0500

    feat(frontend): start building homepage

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WeatherLookupComponent } from '../weather-lookup/weather-lookup.component';
+import { ImageInputComponent } from '../image-input/image-input.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HomePageComponent, WeatherLookupComponent, ImageInputComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
