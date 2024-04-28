44b00793d319fae082933c805f7f7ca5d802b286
Blake <brusseth@asu.edu>
2024-04-27 21:48:10 -0500

    feat(frontend): add app module, continue home page setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,24 +4,19 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { DataActionComponent } from './data-action-component/data-action.component';
-import { ImageInputComponent } from './image-input/image-input.component';
-import { WeatherLookupComponent } from './weather-lookup/weather-lookup.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherLookupComponent,
-    ImageInputComponent,
-    DataActionComponent,
-    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent, HomePageComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: []
 })
 export class AppModule { }
--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherLookupComponent } from '../weather-lookup/weather-lookup.component';
 import { ImageInputComponent } from '../image-input/image-input.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HomePageComponent, WeatherLookupComponent, ImageInputComponent],
+  imports: [CommonModule, HomePageComponent, WeatherLookupComponent, ImageInputComponent, NgbAccordionModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
